4cc7dc6ffd1da0dbf6205a879df01c8173850562
mrjiao <mrjiao2018@163.com>
2019-04-10 22:49:08 +0800

    initialize the repo

---
+++ src/main/java/edu/whu/iss/whufleamarket/WhufleamarketApplication.java
@@ -0,0 +1,13 @@
+package edu.whu.iss.whufleamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhufleamarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhufleamarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/whu/iss/whufleamarket/WhufleamarketApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.whu.iss.whufleamarket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhufleamarketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
