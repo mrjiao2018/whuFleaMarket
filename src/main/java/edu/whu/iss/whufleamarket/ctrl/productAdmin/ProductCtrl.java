a6cf164e6ddc2503ad0f4aeb95279688f73cf31a
mrjiao <mrjiao2018@163.com>
2019-04-26 12:14:59 +0800

    完善 ProductCtrl 的注释

--- src/main/java/edu/whu/iss/whufleamarket/ctrl/productAdmin/ProductCtrl.java
+++ src/main/java/edu/whu/iss/whufleamarket/ctrl/productAdmin/ProductCtrl.java
@@ -17,10 +17,30 @@ public class ProductCtrl {
     @Autowired
     private ProductService productService;
 
+    /**
+     * 用户发布新的二手商品
+     * @param request 网络请求封装 request
+     * @return modelMap 返回内容封装，所有 ctrl 层的方法都必须返回 modelMap ！
+     */
     @RequestMapping(value = "/publish", method = RequestMethod.POST)
     @ResponseBody
     private Map<String, Object> publishSecondHandProduct(HttpServletRequest request){
+        // 在 modelMap 中定义各个字段和对象，
+        // @ResponseBody 会自动将 modelMap 转为 json 字符串返回给前端
         Map<String, Object> modelMap = new HashMap<>();
+
+        try{
+            // 在这里进行 service 层的操作，如 productService.addProduct(...)
+            // productService.addProduct(...)
+
+            // service 操作成功，封装 modelMap 并返回结果
+            modelMap.put("success", true);
+            modelMap.put("errMsg", "");
+        } catch (Exception e){
+            // 如果发生异常，则说明 service 操作失败，封装 modelMap 并返回结果
+            modelMap.put("success", false);
+            modelMap.put("errMsg", e.getMessage());
+        }
 
         return modelMap;
     }
