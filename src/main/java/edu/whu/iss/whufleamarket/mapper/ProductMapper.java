5b6ccb732e939fa7543a6b1edb27030a5cdb615a
mrjiao <mrjiao2018@163.com>
2019-05-09 18:52:15 +0800

    根据类别获取当前所有买卖接口，dao 层完善并进行单元测试

--- src/main/java/edu/whu/iss/whufleamarket/mapper/ProductMapper.java
+++ src/main/java/edu/whu/iss/whufleamarket/mapper/ProductMapper.java
@@ -1,6 +1,9 @@
 package edu.whu.iss.whufleamarket.mapper;
 
 import edu.whu.iss.whufleamarket.vo.Product;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface ProductMapper {
     /**
@@ -10,5 +13,10 @@ public interface ProductMapper {
      */
     int insertProduct(Product product);
 
-
+    /**
+     * 根据类别获取所有买卖
+     * @return
+     */
+    @Select("select * from tb_product where product_category_id = #{productCategoryId}")
+    List<Product> queryAllProducts(Long productCategoryId);
 }
---
+++ src/test/java/edu/whu/iss/whufleamarket/mapper/ProductMapperTest.java
@@ -0,0 +1,20 @@
+package edu.whu.iss.whufleamarket.mapper;
+
+import edu.whu.iss.whufleamarket.WhufleamarketApplicationTests;
+import edu.whu.iss.whufleamarket.vo.Product;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ProductMapperTest extends WhufleamarketApplicationTests {
+    @Autowired
+    ProductMapper productMapper;
+
+    @Test
+    public void queryAllProducts() {
+        List<Product> products = productMapper.queryAllProducts(1l);
+    }
+}
