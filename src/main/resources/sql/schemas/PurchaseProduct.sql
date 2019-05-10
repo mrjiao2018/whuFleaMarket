2575107a3ac9d5a34ed9aba1b49e940555e7cd0c
sxf <785617348@qq.com>
2019-05-10 15:40:59 +0800

    求购sql

--- src/main/resources/sql/schemas/PurchaseProduct.sql
+++ src/main/resources/sql/schemas/PurchaseProduct.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS tb_purchase_product(
-  `purchase_product_id` int(100) NOT NULL AUTO_INCREMENT,
-  `purchase_product_address` varchar(100) NOT NULL,
-  `purchase_product_time` varchar(100) NOT NULL,
-  `purchase_product_content` varchar(100) NOT NULL,
-  `purchase_status` INT(2) NOT NULL DEFAULT 0 COMMENT '0：进行中，1：已完成',
-  `purchase_mode` INT(2) NOT NULL DEFAULT 0 COMMENT '1：代理点，1：自由发布',
-  `purchase_product_category` int(12) NOT NULL,
-  PRIMARY KEY (`purchase_product_id`)
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `time` varchar(100) NOT NULL,
+  `content` varchar(100) NOT NULL,
+  `status` INT(2) NOT NULL DEFAULT 0 COMMENT '0：进行中，1：已完成',
+  `mode` INT(2) NOT NULL DEFAULT 0 COMMENT '1：代理点，1：自由发布',
+  `category` int(12) NOT NULL,
+  PRIMARY KEY (`purchase_product_id`),
+  CONSTRAINT fk_purchase_personinfo FOREIGN KEY (owner_id) REFERENCES tb_person_info(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
